41d8b1378af8fac4676a3e8fd52a350bf38a6e74
Ad3bay0c <aclintonadebayo@gmail.com>
2021-08-28 16:51:20 +0100

    Unbuffered Channel Examples

--- gorourines/channel.go
+++ gorourines/channel.go
@@ -1,1 +1,40 @@
 package gorourines
+
+import (
+	"fmt"
+	"time"
+)
+
+func ChannelsTesting() {
+	fmt.Println("Perform some kind of Channels Testing")
+
+	ch := make(chan interface{})
+
+	go readData(ch)
+	go readData(ch)
+
+	_, _ = <- ch, <-ch
+
+	go getData(ch)
+
+	time.Sleep(time.Second)
+	fmt.Println("End of Main function")
+}
+
+func readData(ch chan interface{}) {
+	ch <- "Good morning"
+	ch <- 24
+	ch <- false
+	ch <- "Goodbye"
+}
+
+func getData(ch chan interface{}) {
+	var input interface{}
+
+	for {
+		input = <- ch
+
+		fmt.Println(input)
+	}
+	close(ch)
+}
