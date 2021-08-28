1a74b02f0df84b7adc4b97d1d980eded0317b10d
Ad3bay0c <aclintonadebayo@gmail.com>
2021-08-28 16:53:06 +0100

    Socket UDP Examples

--- socket/udp.go
+++ socket/udp.go
@@ -1,1 +1,26 @@
 package socket
+
+import (
+	"fmt"
+	"net"
+)
+
+func UDPServer() {
+	service := ":2000"
+	udpAddr, err := net.ResolveUDPAddr("udp", service)
+	checkError(err)
+
+	startUdp, err := net.DialUDP("udp", nil, udpAddr)
+	checkError(err)
+
+	_, err = startUdp.Write([]byte("What's up"))
+	checkError(err)
+
+	server, err := net.ListenUDP("udp", udpAddr)
+	checkError(err)
+
+	buf := make([]byte, 128)
+	_, err = server.Read(buf[0:])
+	checkError(err)
+	fmt.Println(string(buf))
+}
