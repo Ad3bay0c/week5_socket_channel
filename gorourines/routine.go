a0999524425f6d958fc646dcf8e99e9f15cc51ce
Ad3bay0c <aclintonadebayo@gmail.com>
2021-08-28 16:51:55 +0100

    GO routine Examples

--- gorourines/routine.go
+++ gorourines/routine.go
@@ -1,1 +1,34 @@
 package gorourines
+
+import (
+	"flag"
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+func Basic() {
+	fmt.Println(runtime.NumCPU(), runtime.NumGoroutine())
+	fmt.Println("Beginning of main")
+	//go longwait()
+	//time.Sleep(time.Second)
+	fmt.Println("end of main function")
+	fmt.Println(runtime.NumCPU(), runtime.NumGoroutine())
+}
+func longwait(wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Println("Beginning of longwait")
+
+}
+func WaitGroup() {
+	wg := new(sync.WaitGroup)
+	wg.Add(1)
+	go longwait(wg)
+
+	wg.Wait()
+
+	fmt.Println("All finished")
+	numCores := flag.Int("n", 2, "Number of Cores")
+	flag.Parse()
+	fmt.Println(*numCores)
+}
