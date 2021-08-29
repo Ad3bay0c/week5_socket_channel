dac2b97dfbdf0e78de41d6cf9be97635bafb248c
Ad3bay0c <aclintonadebayo@gmail.com>
2021-08-29 23:00:03 +0100

    write message Added

--- implementation/group.go
+++ implementation/group.go
@@ -1,8 +1,21 @@
 package implementation
 
-import "net"
+import (
+	"net"
+	"strings"
+)
 
 type group struct {
 	name 	string
 	members	map[net.Addr]*user
+}
+
+func (g *group) message(user *user, msg []string) {
+	message := strings.Join(msg, " ")
+
+	for key, u := range g.members {
+		if user.conn.RemoteAddr() != key {
+			u.writeMessage(message)
+		}
+	}
 }
