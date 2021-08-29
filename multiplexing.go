196aa2922aaf2ed21b3ff0a31b155899b3ef6fa8
Ad3bay0c <aclintonadebayo@gmail.com>
2021-08-29 21:27:38 +0100

    Multiplexing Done and Dusted

--- multiplexing.go
+++ multiplexing.go
@@ -1,1 +1,26 @@
 package main
+
+type Request struct {
+	a, b	int
+	replyC	chan int
+}
+
+type binOp func(a, b int) int
+
+func Client(op binOp, R *Request) {
+	R.replyC <- op(R.a, R.b)
+}
+
+func Server(op binOp, service chan *Request) {
+	for {
+		req := <- service
+
+		go Client(op, req)
+	}
+}
+
+func StartServer(op binOp) chan *Request {
+	reqChan := make(chan *Request)
+	go Server(op, reqChan)
+	return reqChan
+}
