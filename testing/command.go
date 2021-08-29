0ed76e407da0990a4ce76af84f6cf7df2040a19b
Ad3bay0c <aclintonadebayo@gmail.com>
2021-08-29 21:26:04 +0100

    Different commands Added

--- testing/command.go
+++ testing/command.go
@@ -1,1 +1,17 @@
 package testing
+
+type commandID int
+
+const (
+	CMD_NICK commandID = iota
+	CMD_JOIN
+	CMD_ROOMS
+	CMD_MSG
+	CMD_QUIT
+)
+
+type command struct {
+	id 		commandID
+	client	*client
+	args	[]string
+}
