1b53e014eb96bb6438e7a49cb8a2cdaf5bc694d0
Ad3bay0c <aclintonadebayo@gmail.com>
2021-08-29 21:28:06 +0100

    Build Main and Client Created

--- client.go
+++ client.go
@@ -1,1 +1,9 @@
 package main
+
+import "github.com/Ad3bay0c/week5/implementation"
+
+func main() {
+	//socket.TCPClient()
+
+	implementation.Client()
+}
--- main.go
+++ main.go
@@ -1,1 +1,40 @@
-package week5
+package main
+
+import "github.com/Ad3bay0c/week5/testing"
+
+func main() {
+	//socket.Ip()
+	//socket.RunIt()
+
+	//socket.TCPServer()
+
+	//socket.TCPServer()
+
+	//socket.UDPServer()
+
+	//gorourines.Basic()
+	//gorourines.WaitGroup()
+	//gorourines.ChannelsTesting()
+	//gorourines.BufferedChannels()
+
+	//gorourines.Example()
+
+	//gorourines.SelectTesting()
+	//m := make(chan int, 2)
+	//go func() {
+	//	m <- 24
+	//}()
+	//go func() {
+	//	fmt.Println(<-m)
+	//}()
+	//time.Sleep(time.Second)
+	//fmt.Println("Okay ended")
+
+	//implementation.Server()
+	//fmt.Println(runtime.NumCPU())
+
+	testing.Execute()
+}
+
+
+
