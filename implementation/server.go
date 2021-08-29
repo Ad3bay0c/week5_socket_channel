d6c3016897bd51daee09c4665839b03317c78e45
Ad3bay0c <aclintonadebayo@gmail.com>
2021-08-29 23:39:18 +0100

    Join Group, QuitGroup Method Added

--- implementation/server.go
+++ implementation/server.go
@@ -2,6 +2,7 @@ package implementation
 
 import (
 	"errors"
+	"fmt"
 	"log"
 	"net"
 	"strings"
@@ -54,10 +55,13 @@ func (s *server) readClient() {
 		case USERNAME:
 			s.addUsername(ins.user, ins.args)
 		case JOIN:
+			s.joinGroup(ins.user, ins.args)
 		case REPLY:
 			s.replyMessage(ins.user, ins.args)
 		case GROUPLIST:
+			s.groupList(ins.user)
 		case QUIT:
+			s.quitConnection(ins.user)
 		}
 	}
 }
@@ -65,6 +69,7 @@ func (s *server) readClient() {
 func (s *server) addUsername(user *user, args []string) {
 	if len(args) < 2 {
 		user.errorMessage(errors.New("specify the Username; *name Bay"))
+		return
 	}
 	user.username = strings.TrimSpace(args[1])
 	user.writeMessage("Username Updated: " + user.username)
@@ -73,13 +78,68 @@ func (s *server) addUsername(user *user, args []string) {
 func (s *server) replyMessage(user *user, args []string) {
 	if len(args) < 2 {
 		user.errorMessage(errors.New("type a message; *reply hi"))
+		return
 	}
 
 	if user.group == nil {
-		user.errorMessage(errors.New("please, Join a Group first; *join groupname"))
+		user.errorMessage(errors.New("please, Join a Group first; *join groupName"))
+		return
 	}
-
-	user.group.message(args)
+	msg := strings.Join(args[1:], " ")
+	user.group.message(user, msg)
 }
 
+func (s *server) groupList(user *user) {
+	if len(s.groups) == 0 {
+		user.writeMessage("Empty Group")
+		return
+	}
+	group := ""
+	for _, v := range s.groups {
+		group += v.name + ", "
+	}
 
+	user.writeMessage(group)
+}
+
+func (s *server) quitGroup(user *user)  {
+	if user.group == nil {
+		user.errorMessage(errors.New("please join a group"))
+		return
+	}
+	delete(user.group.members, user.conn.RemoteAddr())
+	user.group.message(user, fmt.Sprintf("%v left the group", user.username))
+}
+
+func (s *server) quitConnection(user *user) {
+	if user.group != nil {
+		s.quitGroup(user)
+	}
+	user.conn.Close()
+	log.Printf("A connectionDisconnected: %v", user.conn.RemoteAddr())
+}
+
+func (s *server) joinGroup(u *user, args []string) {
+	if len(args) < 2 {
+		u.errorMessage(errors.New("enter the group to join"))
+		return
+	}
+	name := strings.TrimSpace(args[0])
+
+	g, ok := s.groups[name]
+	if !ok {
+		g = &group{
+			name: name,
+			members: make(map[net.Addr]*user),
+		}
+
+		s.groups[name] = g
+	}
+
+	g.members[u.conn.RemoteAddr()] = u
+
+	u.group = g
+	u.group.message(u,fmt.Sprintf("%v joined the group", u.username))
+
+	u.writeMessage("Welcome to "+ name)
+}
