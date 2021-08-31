e8be5a89927a6d5bb2b99822de2e459d351264d0
Ad3bay0c <aclintonadebayo@gmail.com>
2021-08-31 02:22:10 +0100

    User Method Adjusted

--- implementation/user.go
+++ implementation/user.go
@@ -10,12 +10,12 @@ import (
 
 type user struct {
 	username		string
-	conn			net.Conn
-	group			*group
+	conn 			net.Conn
+	chat 			*chat
 }
 
 func (user *user) acceptInput(s *server) {
-	user.writeMessage(user,"Type a writeMessage... \n")
+	user.writeMessage(user,"Type a Message... \n")
 	for {
 		input, err := bufio.NewReader(user.conn).ReadString('\n')
 		if err != nil {
@@ -38,9 +38,9 @@ func (user *user) acceptInput(s *server) {
 				args: args,
 				user: user,
 			}
-		case "*grouplist":
+		case "*chats":
 			s.instructions <- instruction{
-				id: GROUPLIST,
+				id: CHATS,
 				args: args,
 				user: user,
 			}
@@ -56,14 +56,21 @@ func (user *user) acceptInput(s *server) {
 				args: args,
 				user: user,
 			}
-		case "*reply":
+		case "*send":
 			s.instructions <- instruction{
-				id: REPLY,
+				id: SEND,
 				args: args,
 				user: user,
 			}
 		default:
-			user.errorMessage(errors.New("invalid command"))
+			user.errorMessage(
+				errors.New(
+					"invalid command; Choose from these commands: \n\t *username 'your name'-> to set your username," +
+						"\n\t *chats -> to list all available chat chats," +
+						"\n\t *join 'chats'-> to join a chat," +
+						"\n\t *send 'message'-> to send a message to a chat," +
+						"\n\t *quit -> to disconnect from a connection",
+					))
 		}
 	}
 }
@@ -72,5 +79,5 @@ func (user *user) writeMessage(u *user, msg string) {
 }
 
 func (user *user) errorMessage(err error) {
-	user.conn.Write([]byte(fmt.Sprintf("OOPS!!! An Error Occurred: %v\n", err)))
+	user.conn.Write([]byte(fmt.Sprintf("OOPS!!! An Error Occurred: \n%v\n", err)))
 }
